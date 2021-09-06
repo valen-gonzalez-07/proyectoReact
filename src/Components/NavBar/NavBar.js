eae472e6d9c1d83f1e20078afb448d724efd62bc
valen-gonzalez-07 <valentina082000.vg@gmail.com>
2021-09-06 20:27:24 -0300

    Probando navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './NavBar.css'
+import './NavBar.css';
 
 const NavBar = () => {
     return (
         <div className="navbar">
-            
+            <div class="logo">Finca las moras</div>
+            <div>
+                <ul>
+                    <li>PAZ</li>
+                    <li>LOS INTOCABLES</li>
+                    <li>ALMA MORA</li>
+                    <li>ALMA MORA SELECT</li>
+                    <li>DADÁ</li>
+                </ul>
+            </div>
         </div>
     )
 }
